5fac0871807afcf2529b27161dcf055061753697
Anbazhakanm <43094992+Anbazhakanm@users.noreply.github.com>
2022-10-11 17:02:26 +0530

    seconf commit

--- main/app.js
+++ main/app.js
@@ -1,24 +1,35 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const spawn = require("child_process").spawn;
+var result = '';
+const { PythonShell } = require('python-shell');
+
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var result = '';
+
+let options = {
+mode: 'text',
+};
 
 app.post("/view", (req, res) => {
 
-	result = req.body;
-	console.log("Entry displayed successfully",result);
-    res.send(result);
+result = req.body;
+pyResponse = '';
+PythonShell.run('form.py', options, function(err, results) {
+if (err) console.log(err);
+pyResponse = results;
+res.send(pyResponse);
 });
-app.get("/view", (req, res) => {
+});
+app.get("/getData", (req, res) => {
 
-	console.log(result);
-    res.send(result);
-    console.log("Success Get Response ", result);
+console.log(result);
+res.send(result);
+console.log("Success Get Response ", result);
 });
 
 // Server listening to PORT 3000
--- main/form.py
+++ main/form.py
@@ -1,15 +1,11 @@
 import requests
-  
-# Sample array
-array = [1,2,3,4,5,6,7,8,9,10]
-  
-# Data that we will send in post request.
-data = {'array':array}
-  
-# The POST request to our node server
-res = requests.get('http://127.0.0.1:3000/view')
-  
-# Convert response data to json
-  
-print('hello', res.content)
 
+# The POST request to our node server
+res = requests.get('http://127.0.0.1:3000/getData')
+# Convert response data to json
+# print('hello', res.content)
+import sys
+
+
+print('Results from ML DEBUGGER',res.content)
+sys.stdout.flush()
